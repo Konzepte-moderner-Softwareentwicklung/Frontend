13be908ca6a55f7e33ea79c3fec2e927e8353031
chrastlet <luca.foerger@gmail.com>
2025-06-11 15:48:43 +0200

    Router works shadcn is nearly done but i still have problems with installing tailwind on windows

---
+++ frontend/pages/Home.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import { Switch } from "@/components/ui/switch.jsx"
+import {Button} from "@/components/ui/button.jsx";
+
+function Home() {
+    const [enabled, setEnabled] = useState(false);
+
+    return (
+        <div className="p-6">
+            <h1 className="text-2xl font-bold mb-4">Home</h1>
+            <div className="flex items-center space-x-4">
+                <Switch checked={enabled} onCheckedChange={setEnabled} />
+                <Button/>
+                <span>{enabled ? "Aktiviert" : "Deaktiviert"}</span>
+            </div>
+        </div>
+    );
+}
+export default Home;
