e2f26c2fdd6b34ec17683c0ff4b2acef08f24a1b
Niklas <niklas.panz@mni.thm.de>
2025-06-14 14:31:58 +0200

    init

--- frontendts/src/App.tsx
+++ frontendts/src/App.tsx
@@ -1,13 +1,16 @@
-import { Button } from "@/components/ui/button"
+
 import Navbar from "./components/navbar"
- 
+import { BrowserRouter } from "react-router-dom";
+import { AppRouter } from "./pages/Router"
+
 function App() {
   return (
     <>
     <Navbar/>
-    <div className="flex min-h-svh flex-col items-center justify-center">
-      <Button>Click me</Button>
-    </div>
+
+      <BrowserRouter>
+        <AppRouter />
+      </BrowserRouter>
     </>
   )
 }
